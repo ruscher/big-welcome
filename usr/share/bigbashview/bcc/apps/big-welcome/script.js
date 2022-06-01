1120421ed9f37beb298a44902ed4f244b97d1a93
ruscher <rruscher@gmail.com>
2022-05-31 23:34:43 -0300

    Add logo and modifications

--- usr/share/bigbashview/bcc/apps/big-welcome/script.js
+++ usr/share/bigbashview/bcc/apps/big-welcome/script.js
@@ -129,15 +129,16 @@ class Wizard {
     this.updateButtonsStatus();
   }
 
+  
   handleNextStepButton() {
     if (this.currentStep === this.stepsQuantity - 1) {
-      this.nextControl.innerHTML = 'Concluído!'; //BOTAO CONCLUIDO
+      this.nextControl.innerHTML = '<svg viewBox="0 0 320 512"><path fill="currentcolor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"/></svg>'; //BOTAO CONCLUIDO
 
       this.nextControl.removeEventListener('click', this.nextControlMoveStepMethod);
       this.nextControl.addEventListener('click', this.concludeControlMoveStepMethod);
       this.nextControl.addEventListener('click', this.wizardConclusionMethod);
     } else {
-      this.nextControl.innerHTML = 'Próxima'; //BOTAO PROXIMO
+      this.nextControl.innerHTML = '<svg viewBox="0 0 320 512"><path fill="currentcolor" d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></svg>'; //BOTAO PROXIMO
       this.nextControl.addEventListener('click', this.nextControlMoveStepMethod);
       this.nextControl.removeEventListener('click', this.concludeControlMoveStepMethod);
       this.nextControl.removeEventListener('click', this.wizardConclusionMethod);
@@ -184,3 +185,69 @@ let buttonPrevious = document.querySelector('.previous');
 
 wizard.addControls(buttonPrevious, buttonNext);
 
+function offset(el) {
+	var rect = el.getBoundingClientRect(),
+	scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+	scrollTop = window.pageYOffset || document.documentElement.scrollTop
+	return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+let timelineLogoScale = new gsap.timeline()
+let timelineInitial = new gsap.timeline()
+let timelineCircle = new gsap.timeline()
+let timelineLogo = new gsap.timeline()
+
+let $btn = document.querySelector('.btn')
+
+let perimeter = 29 * 2 * Math.PI
+let $logoWrapper = document.querySelector('#logo-wrapper')
+let $logo = document.querySelector('#logo-wrapper #logo')
+let $paths = document.querySelectorAll('#logo path')
+let $circleLoader = document.querySelector('#circle-loader')
+let $circle = document.querySelector('#circle-loader circle')
+let $initialAnimation	= document.querySelector('.initial-animation')
+
+let windowWidth	= window.innerWidth;
+let windowHeight = window.innerHeight;
+
+let logoWidth = $logoWrapper.offsetWidth
+let logoHeight = $logoWrapper.offsetHeight
+let positionLogoTop = offset($logoWrapper).top
+let positionLogoLeft = offset($logoWrapper).left
+
+let centerX = (windowWidth - logoWidth) / 2 - positionLogoLeft
+let centerY = (windowHeight - logoHeight) / 2 - positionLogoTop
+
+$logoWrapper.style.transform = 'translate('+ centerX +'px, '+ centerY +'px)'
+
+for(let i = 1; i < $paths.length; i++ ) {
+	$paths[i].style.strokeDasharray = $paths[i].style.strokeDashoffset = $paths[i].getTotalLength()
+}
+
+
+timelineLogo.add([
+	gsap.to($paths[1], 2.1, {strokeDashoffset: 0, delay: .4}),
+	gsap.to($paths[2], 0.6, {strokeDashoffset: 0, delay: .7})
+])
+
+timelineCircle
+	.fromTo($circle, 1.6, 
+      {delay: 1.5, strokeDasharray: '0px ' + perimeter + 'px', ease: 'power2.inOut'}, 
+      {delay: 0.7, strokeDasharray: perimeter + 'px ' + perimeter + 'px', ease: 'power2.inOut'} 
+   )
+	.to($circleLoader, 1.3, {rotationZ: '+=360deg'}, '-=1.6' )
+
+timelineLogoScale
+	.to($logoWrapper, {scale: 1.6})
+	.to($logoWrapper, 0.4, {delay: 0.1, scale: 0.8, ease: 'power4.inOut'})
+	.to($logoWrapper, 0.5, {delay: 0.3, scale: 1, ease: 'power4.inOut'})
+	.to($logoWrapper, 0.2, {delay: 0.2, scale: 0.9, ease: 'power4.inOut'})
+    .to($logoWrapper, 0.2, {delay: 0.1, scale: 1, ease: 'power4.inOut'})    
+
+
+$btn.addEventListener('click', function() {
+  timelineLogoScale.restart()
+  timelineInitial.restart()
+  timelineCircle.restart()
+  timelineLogo.restart()
+})
